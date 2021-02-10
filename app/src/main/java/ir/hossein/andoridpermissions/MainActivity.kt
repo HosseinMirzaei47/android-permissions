1a9d337c53fa4d48fe1f553867373f53f3aec2de
HosseinMirzaei47 <hosseinhidd@gmail.com>
2021-02-10 15:21:40 +0330

    initial project

---
+++ app/src/main/java/ir/hossein/andoridpermissions/MainActivity.kt
@@ -0,0 +1,11 @@
+package ir.hossein.andoridpermissions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
